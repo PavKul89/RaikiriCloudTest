3626a2da52243bff6fe7593d6bcfcb38add48802
Pavel Kulazhanka <PavKul89@gmail.com>
2026-01-03 20:50:17 +0300

    Initial project

---
+++ src/main/java/com/example/testProjectRaikiriCloud/TestProjectRaikiriCloudApplication.java
@@ -0,0 +1,13 @@
+package com.example.testProjectRaikiriCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProjectRaikiriCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestProjectRaikiriCloudApplication.class, args);
+	}
+
+}
