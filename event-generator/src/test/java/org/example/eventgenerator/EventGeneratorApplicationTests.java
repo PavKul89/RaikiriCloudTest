d22599e023e551e338d86e608455efd7ddf2befc
Pavel Kulazhanka <PavKul89@gmail.com>
2026-01-04 11:31:51 +0300

    create structure project

---
+++ event-generator/src/test/java/org/example/eventgenerator/EventGeneratorApplicationTests.java
@@ -0,0 +1,11 @@
+package org.example.eventgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventGeneratorApplicationTests {
+
+
+
+}
---
+++ event-registry/src/test/java/org/example/eventregistry/EventRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.eventregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventRegistryApplicationTests {
+
+//    @Test
+//    void contextLoads() {
+//    }
+
+}
